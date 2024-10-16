949363ac1112865db0e96577ce836377a6ab497b
j25ng <jsan2861@gmail.com>
2024-10-16 11:15:43 +0900

    stack

--- src/main/java/shop/samdul/greeting/controller/GreetingController.java
+++ src/main/java/shop/samdul/greeting/controller/GreetingController.java
@@ -4,13 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import java.util.*;
 
 @Controller
 public class GreetingController {
+    private Stack<String> names = new Stack<>();
+
     @GetMapping("/greeting")
     public String greeting(
             @RequestParam(name = "name", required = false, defaultValue = "HI") String name, Model model) {
-        model.addAttribute("name", name);
+
+        names.add(name);
+        model.addAttribute("names", names);
         return "greeting";
     }
 }
